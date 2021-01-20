f8278d555f1fbb071bf15acc4799d43236f717fa
Tanuj Dey <tanujdey7@gmail.com>
2021-01-21 00:03:28 +0530

    thread programming v1

--- 6.java
+++ 6.java
@@ -1,9 +1,32 @@
-class Account {
+class Account extends Thread {
+    int num;
 
+    public synchronized void access() {
+        num++;
+    }
 }
 
 class demo {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Account a = new Account();
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5000; i++) {
+                    a.access();
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5000; i++) {
+                    a.access();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(a.num);
     }
 }
